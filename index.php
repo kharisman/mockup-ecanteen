bd5b99fc0215784ecdebf9ca386173b6b6da0077
kharisma elan <elsevenwriter@gmail.com>
2023-09-20 16:16:39 +0700

    update

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
             <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
                 <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
                     <div class="icon p-2 me-2">
-                        <img class="img-fluid" src="img/logo_hijau.png" alt="Icon" style="width: 60px; ">
+                        <img class="img-fluid" src="img/logo-hijau.png" alt="Icon" style="width: 60px; ">
                     </div>
                     <h1 class="m-0 text-primary">E CANTEEN</h1>
                 </a>
